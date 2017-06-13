7fc5c277226f0a67bb621681a7e27e94791e4d55
Anton Petkov <anton.petkoff@gmail.com>
2017-06-14 00:18:17 +0300

    Recieve Erlang messages instead of calling :gen_tcp.recv

--- apps/client/lib/client/client.ex
+++ apps/client/lib/client/client.ex
@@ -9,26 +9,30 @@ defmodule Client do
   def init(args) do
     host = args[:host]
     port = args[:port]
-    options = [:binary, active: false, packet: :line]
+    options = [:binary, active: true, packet: :line]
 
     Logger.info "Client connecting to #{host}@#{port}..."
     {:ok, server} = :gen_tcp.connect(host, port, options)
 
     Logger.info "Client connected to #{host}@#{port}"
-    {:ok, %{socket: server}}
+    {:ok, %{socket: server, from: nil}}
   end
 
   def execute(command) do
     GenServer.call(__MODULE__, {:execute, command})
   end
 
-  def handle_call({:execute, command}, _form, %{socket: socket} = state) do
+  def handle_info({:tcp, socket, msg}, %{socket: socket} = state) do
+    Logger.info "Received #{msg}"
+    GenServer.reply(state.from, msg)
+    {:noreply, state}
+  end
+
+  def handle_call({:execute, command}, from, %{socket: socket} = state) do
     :ok = :gen_tcp.send(socket, command)
     Logger.info "Sent #{command}"
-
-    {:ok, response} = :gen_tcp.recv(socket, 0)
-    Logger.info "Received #{response}"
-
-    {:reply, {:ok, response}, state}
+    # TODO: we could use a :queue to store "from" for each request (command)
+    #       but the client is just a single process and "from" is always the same
+    {:noreply, %{state | from: from}}
   end
 end
