a2b9298eae057a1cf9b7d2d3d1b09edbab0657d6
Anton Petkov <anton.petkoff@gmail.com>
2017-06-15 11:10:11 +0300

    Client can read a file into Base64 encoded chunks

--- apps/client/lib/client/api.ex
+++ apps/client/lib/client/api.ex
@@ -5,11 +5,29 @@ defmodule Client.API do
   end
 
   def handle({:send_file, username, filename}) do
-    IO.puts "i must read #{filename}"
+    chunks = read_chunks(filename, 512)
+    chunks_count = Enum.count chunks
+    IO.puts "#{chunks_count} chunks read"
+
+    chunks = Enum.map(chunks, &Base.encode64/1)
+    IO.inspect chunks
+
+    # TODO:
+    # ask server for where to send the files
+    # connect to destination
+    # send packages
+
     :ok
   end
 
   def handle(_) do
     {:error, :not_implemented}
+  end
+
+  defp read_chunks(filename, chunk_size) do
+    {:ok, file} = File.open(filename, [:read])
+
+    Stream.repeatedly(fn -> IO.binread(file, chunk_size) end)
+    |> Enum.take_while(fn bytes -> not match?(:eof, bytes) end)
   end
 end
